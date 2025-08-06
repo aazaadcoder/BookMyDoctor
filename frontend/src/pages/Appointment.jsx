75470203df0565e3ce84cccf4d6053a05a88d325
aazaadcoder <128458378+aazaadcoder@users.noreply.github.com>
2025-08-06 19:14:14 +0530

    add the frontend for the navbar

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Appointment = () => {
   return (
     <div>
-      Home Page
+      
     </div>
   )
 }
 
-export default Home
+export default Appointment
