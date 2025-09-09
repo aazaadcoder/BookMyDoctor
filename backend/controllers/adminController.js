2d6a3cffb8fa3fcbe58bfa8b8d2d8a0f5cf7e3b1
aazaadcoder <128458378+aazaadcoder@users.noreply.github.com>
2025-09-09 19:32:10 +0530

    add controller to get data of all doctors

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -116,4 +116,17 @@ const loginAdmin = async (req, res) => {
   }
 };
 
-export { addDoctor, loginAdmin };
+// to get all doctors
+const getAllDoctors = async(req, res) => {
+  try {
+
+    const doctors = await doctorModel.find({}).select('-password');
+    res.json({success : true, doctors});
+
+  } catch (error) {
+    console.log(error);
+    res.json({success: false , message : error.message});
+  }
+}
+
+export { addDoctor, loginAdmin , getAllDoctors};
